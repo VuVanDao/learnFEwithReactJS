2663e912f5f62e96e577e1147853d97c75938c15
Van Dao <dao28901@gmail.com>
2024-10-30 22:09:17 +0700

    #99 Hiển Thị Danh Sách Bệnh Nhân Đặt Lịch Khám Bệnh Cho Bác S

--- src/containers/System/Doctor/ManagePatient.js
+++ src/containers/System/Doctor/ManagePatient.js
@@ -1,23 +1,57 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { LANGUAGES } from "../../../utils/";
-import { FormattedMessage } from "react-intl";
+import { FormattedDate, FormattedMessage } from "react-intl";
 import "./ManagePatient.scss";
 import DatePicker from "../../../components/Input/DatePicker";
-
+import { getAllPatientForDoctor } from "../../../services/userService";
+import moment from "moment";
 class ManagePatient extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentDate: new Date(),
+      currentDate: moment(new Date()).startOf("day").valueOf(),
+      dataPatient: [],
     };
   }
+  async componentDidMount() {
+    let { user } = this.props;
+    let { currentDate } = this.state;
+    let formatedDate = new Date(currentDate).getTime();
+    this.getDataPatient(user, formatedDate);
+  }
+
+  getDataPatient = async (user, formatedDate) => {
+    let res = await getAllPatientForDoctor({
+      doctorId: user.id,
+      date: formatedDate,
+    });
+    if (res && res.errCode === 0) {
+      this.setState({
+        dataPatient: res.data,
+      });
+    }
+  };
 
   async componentDidUpdate(prevProps, prevState) {
     if (this.props.language !== prevProps.language) {
     }
   }
+  HandleOnChangeDatePicker = (date) => {
+    this.setState(
+      {
+        currentDate: date[0],
+      },
+      () => {
+        let { user } = this.props;
+        let { currentDate } = this.state;
+        let formatedDate = new Date(currentDate).getTime();
+        this.getDataPatient(user, formatedDate);
+      }
+    );
+  };
   render() {
+    let { dataPatient } = this.state;
     return (
       <div className="manage-patient-container">
         <div className="m-p-title">Quản lí bệnh nhân khám bệnh</div>
@@ -31,42 +65,46 @@ class ManagePatient extends Component {
             />
           </div>
           <div className="col-12  table-manage-patient">
-            <table style={{ width: "100%" }}>
-              <tr>
-                <th>Company</th>
-                <th>Contact</th>
-                <th>Country</th>
-              </tr>
-              <tr>
-                <td>Alfreds Futterkiste</td>
-                <td>Maria Anders</td>
-                <td>Germany</td>
-              </tr>
-              <tr>
-                <td>Centro comercial Moctezuma</td>
-                <td>Francisco Chang</td>
-                <td>Mexico</td>
-              </tr>
-              <tr>
-                <td>Ernst Handel</td>
-                <td>Roland Mendel</td>
-                <td>Austria</td>
-              </tr>
-              <tr>
-                <td>Island Trading</td>
-                <td>Helen Bennett</td>
-                <td>UK</td>
-              </tr>
-              <tr>
-                <td>Laughing Bacchus Winecellars</td>
-                <td>Yoshi Tannamuri</td>
-                <td>Canada</td>
-              </tr>
-              <tr>
-                <td>Magazzini Alimentari Riuniti</td>
-                <td>Giovanni Rovelli</td>
-                <td>Italy</td>
-              </tr>
+            <table style={{ width: "100%" }} className="">
+              <tbody>
+                <tr>
+                  <th>STT</th>
+                  <th>Time</th>
+                  <th>Name</th>
+                  <th>Address</th>
+                  <th>Gender</th>
+                  <th>Action</th>
+                </tr>
+                {dataPatient && dataPatient.length > 0 ? (
+                  dataPatient.map((item, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{item.timeTypeDataPatient.valueVi}</td>
+                        <td>{item.patientData.firstName}</td>
+                        <td>{item.patientData.address}</td>
+                        <td>{item.patientData.genderData.valueVi}</td>
+                        <td>
+                          <button
+                            className="btn btn-success"
+                            onClick={() => this.handleBtnConfirm()}
+                          >
+                            Confirm
+                          </button>
+                          <button
+                            className="btn btn-primary mx-3"
+                            onClick={() => this.handleBtnRemedy()}
+                          >
+                            Send bill
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })
+                ) : (
+                  <th colSpan={"6"}>Not found</th>
+                )}
+              </tbody>
             </table>
           </div>
         </div>
@@ -78,6 +116,7 @@ class ManagePatient extends Component {
 const mapStateToProps = (state) => {
   return {
     language: state.app.language,
+    user: state.user.userInfo,
   };
 };
 
